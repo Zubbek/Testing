be77653c452ad2557ca48890cf00cf17cc4a8b4e
Zubbek <michal.zub@outlook.com>
2025-02-07 16:10:03 +0100

    create test for sorting products on the page form Z-A

--- tests/test_sorting_products.py
+++ tests/test_sorting_products.py
@@ -23,9 +23,6 @@ def test_sort_products_az():
 
         product_names = [product.text for product in products]
 
-        for product_name in product_names:
-            print(product_name)
-
         assert product_names == sorted(product_names), "Products on the page are not sorted from A-Z"
 
     except Exception as error:
@@ -36,3 +33,31 @@ def test_sort_products_az():
         driver.quit()
 
 
+def test_sort_products_za():
+    driver = chrome_driver()
+    try:
+        driver.get("https://www.saucedemo.com/")
+
+        username = driver.find_element(By.ID, "user-name")
+        password = driver.find_element(By.ID, "password")
+        login_button = driver.find_element(By.ID, "login-button")
+
+        username.send_keys(USERNAME)
+        password.send_keys(PASSWORD)
+        login_button.click()
+
+        dropdown = Select(driver.find_element("class name", "product_sort_container"))
+        dropdown.select_by_value("za")
+
+        products = driver.find_elements("css selector", ".inventory_item_name")
+
+        product_names = [product.text for product in products]
+
+        assert product_names == sorted(product_names, reverse=True), "Products on the page are not sorted from Z-A"
+
+    except Exception as error:
+        print("error occurred: ", error)
+        raise
+
+    finally:
+        driver.quit()
