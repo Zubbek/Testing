0f83183e2414bdb12d671174442ba74e73eb02e6
Zubbek <michal.zub@outlook.com>
2025-02-07 15:54:50 +0100

    create test for sorting products on the page form A-Z

---
+++ tests/test_sorting_products.py
@@ -0,0 +1,38 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
+from utilities.driver_setup import chrome_driver
+from utilities.credentials import USERNAME, PASSWORD
+
+def test_sort_products_az():
+    driver = chrome_driver()
+    try:
+        driver.get("https://www.saucedemo.com/")
+
+        username = driver.find_element(By.ID, "user-name")
+        password = driver.find_element(By.ID, "password")
+        login_button = driver.find_element(By.ID, "login-button")
+
+        username.send_keys(USERNAME)
+        password.send_keys(PASSWORD)
+        login_button.click()
+
+        dropdown = Select(driver.find_element("class name", "product_sort_container"))
+        dropdown.select_by_value("az")
+
+        products = driver.find_elements("css selector", ".inventory_item_name")
+
+        product_names = [product.text for product in products]
+
+        for product_name in product_names:
+            print(product_name)
+
+        assert product_names == sorted(product_names), "Products on the page are not sorted from A-Z"
+
+    except Exception as error:
+        print("error occurred: ", error)
+        raise
+
+    finally:
+        driver.quit()
+
+
