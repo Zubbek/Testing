1d3063df64a9db77f2a0a651f0891a5083be0493
Zubbek <michal.zub@outlook.com>
2025-02-08 15:29:37 +0100

    create test for sorting products on the page from the highest price

--- tests/test_sorting_products.py
+++ tests/test_sorting_products.py
@@ -92,3 +92,34 @@ def test_sort_products_pirce_low():
 
     finally:
         driver.quit()
+
+def test_sort_products_pirce_high():
+    driver = chrome_driver()
+    try:
+        driver.get("https://www.saucedemo.com/")
+
+        username = driver.find_element(By.ID, "user-name")
+        password = driver.find_element(By.ID, "password")
+        login_button = driver.find_element(By.ID, "login-button")
+
+        username.send_keys(USERNAME)
+        password.send_keys(PASSWORD)
+        login_button.click()
+
+        dropdown = Select(driver.find_element("class name", "product_sort_container"))
+        dropdown.select_by_value("hilo")
+
+        products = driver.find_elements("css selector", ".inventory_item_price")
+
+        product_price = [float(product.text.replace("$", "")) for product in products]
+
+        print("products: ", product_price)
+
+        assert product_price == sorted(product_price, reverse=True), "Products on the page are not sorted from the highest Price"
+
+    except Exception as error:
+        print("error occurred: ", error)
+        raise
+
+    finally:
+        driver.quit()
